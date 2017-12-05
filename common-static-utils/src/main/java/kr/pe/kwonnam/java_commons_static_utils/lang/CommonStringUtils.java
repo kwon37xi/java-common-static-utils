9411444454159b3da7c280bc0c189b9a41a4140d
KwonNam Son <kwon37xi@gmail.com>
2017-12-05 22:23:34 +0900

    CommonStringUtils str todo

--- common-static-utils/src/main/java/kr/pe/kwonnam/java_commons_static_utils/lang/CommonStringUtils.java
+++ common-static-utils/src/main/java/kr/pe/kwonnam/java_commons_static_utils/lang/CommonStringUtils.java
@@ -7,14 +7,27 @@ public abstract class CommonStringUtils {
 
     /**
      * null safe trim.
+     *
      * @param str string to trim
      * @return if str is null return null or trim the str
      */
-    public static final String strTrim(String str) {
+    public static String strTrim(String str) {
         if (str == null) {
             return null;
         }
 
         return str.trim();
+    }
+
+    public static boolean strIsEmpty(String str) {
+        return false;
+    }
+
+    public static boolean strIsBlank(String str) {
+        return false;
+    }
+
+    public static int strLength(String str) {
+        return 0;
     }
 }
